6518a09581542ac78a1ec06725d41bc7c98d65b5
guopeng li <guopeng.li@technicolor.com>
2020-12-16 10:33:52 +0100

    Create operator to create observable with objects

--- app/src/main/java/com/lig/intermediate/rxdemo/MainActivity.java
+++ app/src/main/java/com/lig/intermediate/rxdemo/MainActivity.java
@@ -7,9 +7,13 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
@@ -22,15 +26,12 @@ import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 public class MainActivity extends AppCompatActivity {
     private String[] greetings= {"Hello A", "Hello B", "Hello C"};
     private Integer[] nums={1,2,3,4,5};
-    private Observable<Integer> myObservable;
+    private Observable<Student> myObservable;
+    private DisposableObserver<Student> myObserver;
 
-    private DisposableObserver<Integer> myObserver;
-    private DisposableObserver<Integer> myObserver2;
-
-
-    //private Disposable disposable;
     private TextView textView;
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,38 +40,83 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.tvGretting);
 
         //myObservable = Observable.just("Hello 1", "Hello 2", "Hello 3");
-        //myObservable = Observable.fromArray(nums);
-        myObservable = Observable.range(1, 20);
+        //myObservable = Observable.fromArray(getStudents());
+        //myObservable = Observable.range(1, 20);
+
+        myObservable = Observable.create(emitter -> {
+            ArrayList<Student> students = getStudents();
+            for (Student student : students) {
+                emitter.onNext(student);
+            }
+            emitter.onComplete();
+        });
 
         compositeDisposable.add(
-        myObservable
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(getObserver()) // return a observer
+                myObservable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(getObserver()) // return a observer
         );
+    }
 
-        myObserver2 = new DisposableObserver<Integer>() {
+        private DisposableObserver getObserver() {
+            myObserver = new DisposableObserver<Student>() {
+                @Override
+                public void onNext(Student student) {
+                    Log.i("RXdemo", "onNext" + student.getEmail());
+                }
 
-            @Override
-            public void onNext(Integer string) {
-                Log.i("RXdemo", "onNext" + string);
-            }
+                @Override
+                public void onError(@NonNull Throwable e) {
+                    Log.i("RXdemo", "onError");
 
-            @Override
-            public void onError(@NonNull Throwable e) {
-                Log.i("RXdemo", "onError");
+                }
 
-            }
+                @Override
+                public void onComplete() {
+                    Log.i("RXdemo", "onComplete");
 
-            @Override
-            public void onComplete() {
-                Log.i("RXdemo", "onComplete");
+                }
+            };
+            return myObserver;
 
-            }
-        };
-        compositeDisposable.add(
-                myObservable.subscribeWith(myObserver2) // return a observer
-        );
+        }
+
+    private ArrayList<Student> getStudents() {
+
+        ArrayList<Student> students = new ArrayList<>();
+
+        Student student1 = new Student();
+        student1.setName(" student 1");
+        student1.setEmail(" student1@gmail.com ");
+        student1.setAge(27);
+        students.add(student1);
+
+        Student student2 = new Student();
+        student2.setName(" student 2");
+        student2.setEmail(" student2@gmail.com ");
+        student2.setAge(20);
+        students.add(student2);
+
+        Student student3 = new Student();
+        student3.setName(" student 3");
+        student3.setEmail(" student3@gmail.com ");
+        student3.setAge(20);
+        students.add(student3);
+
+        Student student4 = new Student();
+        student4.setName(" student 4");
+        student4.setEmail(" student4@gmail.com ");
+        student4.setAge(20);
+        students.add(student4);
+
+        Student student5 = new Student();
+        student5.setName(" student 5");
+        student5.setEmail(" student5@gmail.com ");
+        student5.setAge(20);
+        students.add(student5);
+
+        return students;
     }
 
     @Override
@@ -79,26 +125,5 @@ public class MainActivity extends AppCompatActivity {
         compositeDisposable.clear(); // clear the disposables held, if use dispose will destroy the composite
     }
 
-    private DisposableObserver getObserver(){
-        myObserver = new DisposableObserver<Integer>() {
-            @Override
-            public void onNext(Integer string) {
-                Log.i("RXdemo", "onNext" + string);
-            }
-
-            @Override
-            public void onError(@NonNull Throwable e) {
-                Log.i("RXdemo", "onError");
-
-            }
-
-            @Override
-            public void onComplete() {
-                Log.i("RXdemo", "onComplete");
-
-            }
-        };
-
-        return myObserver;
-    }
 }
+
